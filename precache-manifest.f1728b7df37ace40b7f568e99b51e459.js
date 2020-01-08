beaf13e3f021c5d7180aded63f60c0f4a70423c2
Aleksey Kryukov <Hed@localhost.localdomain>
2020-01-08 13:37:22 +0300

    Updates

---
+++ precache-manifest.f1728b7df37ace40b7f568e99b51e459.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bd3ec5ae9597927a7cea59bb0f088f73",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "f4f6a7e4d49f8bc87da5",
+    "url": "/monsters-rolodex/static/css/main.12d4656b.chunk.css"
+  },
+  {
+    "revision": "55ee059b2988e93ca287",
+    "url": "/monsters-rolodex/static/js/2.279358c6.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/monsters-rolodex/static/js/2.279358c6.chunk.js.LICENSE"
+  },
+  {
+    "revision": "f4f6a7e4d49f8bc87da5",
+    "url": "/monsters-rolodex/static/js/main.39466914.chunk.js"
+  },
+  {
+    "revision": "d90ac9b9402da2dda087",
+    "url": "/monsters-rolodex/static/js/runtime-main.f8e4036a.js"
+  }
+]);
